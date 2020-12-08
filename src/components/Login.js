b31d150700f6c90d7ab7be7b9e24a611a6c1de68
andrew <andrewnijmeh1@gmail.com>
2020-12-08 07:21:04 -0500

    Start add error handling

---
+++ src/components/Login.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {useState} from 'react'
+import {useEffect} from 'react'
+import {useAuth} from '../contexts/AuthContext'
+import {Link, useHistory} from 'react-router-dom'
+import {useRef} from "react"
+export default function Login({children}) {
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const {login} = useAuth()
+    const [error, setErrors] = useState('')
+    const [loading, setLoading] = useState(false)
+
+    async function handleSubmit(e){
+        e.preventDefault()
+    }
+
+    try{
+        setError('')
+        setLoading(true)
+        await login(emailRef.current.value, passwordRef.current.value)
+        history.push('/')
+
+
+    } catch{
+        setError("Failed to log in :/")
+    }
+    return(
+
+    )
+}
