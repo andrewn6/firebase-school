3b5f17a4b0045a64c4f2705718a7a3f0636f2d70
Andrew Nijmeh <andrewnijmeh1@gmail.com>
2020-12-28 13:19:53 -0500

    add nav

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import Navbar from 'react-bootstrap';
+import Navbar, {Form, Nav} from 'react-bootstrap';
 import AuthProvider from "../contexts/AuthContext";
 import Router from 'react-dom';
+import {Login} from '../components/Login'
+import { Signup } from '../components/Signup'
+
 function App() {
   return (
     <>
+      <Navbar bg="light" variant="light">
+        <Navbar.Brand href='/'>Firebase App</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="/login" component={Login}>Login</Nav.Link>
+          <Nav.Link href="/signup" component={Signup}>Signup</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-primary">Search</Button>
+        </Form>
+      </Navbar>
     </>
+
   )
 
 }
